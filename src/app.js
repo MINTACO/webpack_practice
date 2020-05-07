edddb4607714d1fa49c45396ecc38ccf413e61fc
MINTACO <734876157@qq.com>
2020-05-07 15:03:05 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import sum from './modules/sum';
+var minus = require('./modules/minus');
+
+
+// console.log(sum(1,2), minus(2,1));
+console.log('app')
---
+++ src/treeShaking.js
@@ -0,0 +1,7 @@
+import lodash from 'lodash-es';
+
+function isArray(ele){
+    console.log(lodash.isArray(ele));
+}
+
+isArray([1,2,3]);
